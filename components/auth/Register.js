37cd2d018036e1a6acc827ab5a2451a29d8c3b72
MWest2020 <mwesterweel@hotmail.com>
2021-02-15 01:32:56 +0100

    basic buttons 32:54

---
+++ components/auth/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Button, TextInput } from 'react-native';
+
+export class Register extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Register
